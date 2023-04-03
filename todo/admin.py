862f8f8207f35a3a66391e805321f5479703c8f0
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-04-03 10:57:13 +0300

    change admin

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from todo.models import Task, Tag
+
+admin.site.unregister(Group)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "content",
+        "created_at",
+        "deadline",
+        "status",
+    )
+    search_fields = ("content", "deadline",)
+    list_filter = ("content", "deadline", "status",)
+
+
+admin.site.register(Tag)

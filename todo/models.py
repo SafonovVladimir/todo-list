bafde93a4e08612be01c3616d79081c5ce6727fd
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-04-03 10:34:37 +0300

    Add models

---
+++ todo/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    status = models.BooleanField()
+    tags = models.ManyToManyField("Tag", related_name="tasks")
+
+    def __str__(self):
+        return self.content
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=63)
+
+    def __str__(self):
+        return self.name
